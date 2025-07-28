3618683d843df4510586707c0956ca9847186611
souravpan140 <souravpansourav1@gmail.com>
2025-07-28 13:42:18 +0530

    Create odd-even.cpp

---
+++ odd-even.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    if(n%2==0){
+        cout<<"the number is even";
+    }
+    else{
+        cout<<"trhe number is odd";
+    }
+    return 0;
+}
